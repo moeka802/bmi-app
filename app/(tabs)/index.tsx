a95e5611cc57fad941478a6b03206fe1eb256afe
moeka802 <moeka802.redapple@gmail.com>
2024-12-14 00:02:04 +0900

    要素を追加

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { calculateBMI } from "@/utils/calculateBMI";
 
@@ -7,7 +7,27 @@ export default function HomeScreen() {
     <SafeAreaView
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <Text>Hello World</Text>
+      <Text>BMI</Text>
+      <Text>Body Mass Index</Text>
+      <View>
+        <Text>height</Text>
+        <TextInput placeholder="height" />
+      </View>
+      <View>
+        <Text>weight</Text>
+        <TextInput placeholder="weight" />
+      </View>
+
+      <View>
+        <Text>あなたは...</Text>
+        <Text>普通体重</Text>
+        <Text>（BMI: 24）</Text>
+        <Text>です。</Text>
+      </View>
+
+      <View>
+        <Text>あと2.7kg減らすと、適正体重63.3kgになります。</Text>
+      </View>
     </SafeAreaView>
   );
 }
