e84d5e95983dc5b2a84dfa57645869c0747fa035
moeka802 <moeka802.redapple@gmail.com>
2024-12-13 23:01:42 +0900

    検証用コードを追加

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,8 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { calculateBMI } from "@/utils/calculateBMI";
 
 export default function HomeScreen() {
-  const calculateBMI = (props: {
-    /**
-     * 性別
-     */
-    gender?: "female" | "male";
-    /**
-     * 身長
-     */
-    height: number;
-    /**
-     * 体重
-     */
-    weight: number;
-    /**
-     * 年齢
-     */
-    age?: number;
-  }) => {
-    // BMIの計算処理
-    return 0;
-  };
-
   return (
     <SafeAreaView
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
---
+++ utils/calculateBMI.ts
@@ -0,0 +1,21 @@
+export const calculateBMI = (props: {
+  /**
+   * 性別
+   */
+  gender?: "female" | "male";
+  /**
+   * 身長
+   */
+  height: number;
+  /**
+   * 体重
+   */
+  weight: number;
+  /**
+   * 年齢
+   */
+  age?: number;
+}) => {
+  // BMIの計算処理
+  return 20;
+};
